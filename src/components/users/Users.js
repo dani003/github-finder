6277af8b64e2f892d09038e1444b624aa26c8229
Daniela <daniela.ramirez003@gmail.com>
2021-06-22 22:08:18 -0400

    show users & show loading

---
+++ src/components/users/Users.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import UserItem from './UserItem'
+
+ const Users = ({users, loading}) => {
+    return (
+        <div style={userStyle}>
+            {users.map(user => (
+                <UserItem key={user.id} user={user}></UserItem>
+            ))}
+        </div>
+    )
+}
+
+const userStyle = {
+    display:'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
+}
+export default Users
