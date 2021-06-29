33abcc337ee80c04f88b7de9f52726cf1efc7b2c
Daniela <daniela.ramirez003@gmail.com>
2021-06-28 22:05:47 -0400

    Search & alert

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from './components/layouts/Navbar';
+import Alert from './components/layouts/Alert';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 
 import './App.css';
@@ -10,24 +12,61 @@ class App extends Component {
 
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
-  async componentDidMount(){
+  /*async componentDidMount(){
     this.setState({loading: true});
 
-    const res = await axios.get('https://api.github.com/users')
+    const res = await axios.get(`https://api.github.com/users?client_id=$
+    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({users: res.data, loading:false});
+  }*/
+
+  // Search Github Users
+  searchUsers = async text =>{
+    this.setState({ loading:true });
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${
+      process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${
+      process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({users: res.data.items, loading:false});
   }
+
+  //  Clear users fromm state
+  clearUsers = () => {
+    this.setState({users: [], loading:false});
+  }
+
+  //
+  setAlert = (msg, type) => {
+    this.setState({ alert: {msg, type}});
+
+    setTimeout(() => this.setState({alert:null}), 2000);
+  }
+
   render() { 
-    return (
+
+    const { users, loading } = this.state;
+
+     return (
       <div className="App">
         <Navbar></Navbar>
         <div className="container">
-          <Users loading={this.state.loading} users={this.state.users}></Users>
+          <Alert alert={this.state.alert}></Alert>
+          <Search 
+            searchUsers={this.searchUsers} 
+            clearUsers={this.clearUsers} 
+            showClear={ users.length > 0 ? true : false}
+            setAlert={this.setAlert}>
+            </Search>
+          <Users loading={loading} users={users}></Users>
         </div>
-      </div>
+      </div> 
     );
   }
 }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layouts/Spinner';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 
 
  const Users = ({users, loading}) => {
@@ -19,9 +19,9 @@ import PropTypes from 'prop-types';
     
 }
 
-Users.PropTypes ={
-    users: PropTypes.array.isRequired,
-    loading: PropTypes.bool.isRequired,
+Users.propTypes ={
+    users: propTypes.array.isRequired,
+    loading: propTypes.bool.isRequired,
 }
 
 const userStyle = {
